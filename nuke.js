b0c0496c404e2d32f6bc40951039a25872d2d166
Iraq1337 <71925619+Iraq1337@users.noreply.github.com>
2021-08-11 02:00:14 +0200

    Add files via upload

---
+++ nuke.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const randomcolor = require('randomcolor');
+function nuke(msg)
+{
+    msg.channel.clone().then(new_channel =>{
+        new_channel.setPosition(msg.channel.position)
+        new_channel.send('https://tenor.com/view/nuke-automic-boom-boom-nuked-gif-19961792')
+        msg.channel.delete();
+    })
+}
+module.exports.nuke = nuke;
