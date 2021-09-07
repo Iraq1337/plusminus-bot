7239edc3a2233fd984dbf6ea35fbf64bb2fe90f1
Iraq1337 <71925619+Iraq1337@users.noreply.github.com>
2021-09-07 20:47:11 +0200

    Delete commands directory

--- commands/nuke.js
+++
@@ -1,9 +0,0 @@
-const { Client, Collection, MessageEmbed, Intents, MessageActionRow, MessageButton, Message } = require('discord.js');
-
-module.exports.run = (client, message, args) => {
-    message.channel.clone().then(newchannel => {
-        newchannel.setPosition(message.channel.position);
-        message.channel.delete()
-        newchannel.send("aga")
-    });
-};
